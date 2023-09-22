d1731182ce205247ba5ac54afef57bcd4531aa30
Jefri Achmad Maulana <jefriachmadm@gmail.com>
2023-09-22 11:14:18 +0700

    create delete method

--- src/handler.js
+++ src/handler.js
@@ -43,10 +43,10 @@ const getAllNotesHandler = () => ({
 
 const getNoteByIdHandler = (request, h) => {
   const { id } = request.params;
- 
+
   const note = notes.filter((n) => n.id === id)[0];
- 
- if (note !== undefined) {
+
+  if (note !== undefined) {
     return {
       status: 'success',
       data: {
@@ -64,12 +64,12 @@ const getNoteByIdHandler = (request, h) => {
 
 const editNoteByIdHandler = (request, h) => {
   const { id } = request.params;
- 
+
   const { title, tags, body } = request.payload;
   const updatedAt = new Date().toISOString();
- 
+
   const index = notes.findIndex((note) => note.id === id);
- 
+
   if (index !== -1) {
     notes[index] = {
       ...notes[index],
@@ -78,7 +78,7 @@ const editNoteByIdHandler = (request, h) => {
       body,
       updatedAt,
     };
- 
+
     const response = h.response({
       status: 'success',
       message: 'Catatan berhasil diperbarui',
@@ -86,7 +86,7 @@ const editNoteByIdHandler = (request, h) => {
     response.code(200);
     return response;
   }
- 
+
   const response = h.response({
     status: 'fail',
     message: 'Gagal memperbarui catatan. Id tidak ditemukan',
@@ -94,4 +94,32 @@ const editNoteByIdHandler = (request, h) => {
   response.code(404);
   return response;
 };
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler };
+
+const deleteNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+module.exports = {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler };
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 /* eslint-disable linebreak-style */
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
 const routes = [
   {
     method: 'POST',
@@ -20,6 +20,11 @@ const routes = [
     method: 'PUT',
     path: '/notes/{id}',
     handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
   },
 ];
 
